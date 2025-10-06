50e9365ae762abfffbfccc4e8478bbdfc34ed6a7
pyounghwa.yoon <pyounghwa.yoon@hlcompany.com>
2025-10-07 03:45:15 +0900

    implement Parser

--- Parser/parser.py
+++ Parser/parser.py
@@ -40,17 +40,28 @@ class Parser:
     def arg1(self) -> str:
         assert self.currentCmdType == CommandType.C_RETURN, \
             "This method cannot be used if the current command type is C_RETURN."
-
+        if self.currentCmdType == CommandType.C_ARITHMETIC:
+            return self.currentCmd
+        elif self.currentCmdType == CommandType.C_PUSH:
+            return self.currentCmd.split(" ")[1]
+        elif self.currentCmdType == CommandType.C_POP:
+            return self.currentCmd.split(" ")[1]
+        else:
+            raise AssertionError("An unknown command type was read.")
 
     def arg2(self) -> int:
-        assert self.currentCmdType not in [CommandType.C_PUSH, CommandType.C_POP, CommandType.C_FUNCTION], \
+        assert self.currentCmdType not in [CommandType.C_PUSH, CommandType.C_POP, CommandType.C_FUNCTION, CommandType.C_CALL], \
             "This method cannot be used if the current command type is not C_PUSH, C_POP, C_FUNCTION."
-        
+        if self.currentCmdType == CommandType.C_PUSH:
+            return self.currentCmd.split(" ")[2]
+        elif self.currentCmdType == CommandType.C_POP:
+            return self.currentCmd.split(" ")[2]
+        else:
+            raise AssertionError("An unknown command type was read.")
+    
     def __isCommand(self) -> bool:
 
         if self.currentCmd.startswith("//") or self.currentCmd == "":
-            ret = False
+            return False
         else:
-            ret = True
-
-        return ret
+            return True
