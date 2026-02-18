1e67bb00767892ceacd5994575b27cb05f7076f7
peaceYoon <peace@peaceYoonui-MacBookPro.local>
2026-02-19 02:22:54 +0900

    fix arg1, arg2 error Label Parser 추가

--- Parser/parser.py
+++ Parser/parser.py
@@ -30,31 +30,30 @@ class Parser:
     def commandType(self) -> CommandType:
         assert self.currentCmd, "This method cannot be used if the current command type is empty"
 
-
-
         if self.currentCmd.split(" ")[0] in ["add", "sub", "neg", "eq", "gt", "lt", "and", "or", "not"]:
             return CommandType.C_ARITHMETIC
         elif self.currentCmd.split(" ")[0] == "push":
             return CommandType.C_PUSH
         elif self.currentCmd.split(" ")[0] == "pop":
             return CommandType.C_POP
+        elif self.currentCmd.split(" ")[0] == "label":
+            return CommandType.C_LABEL
         else:
             raise AssertionError("An unknown command type:{!r} was read.".format(self.currentCmd))
         
     def arg1(self) -> str:
-        assert self.currentCmdType == CommandType.C_RETURN, \
-            "This method cannot be used if the current command type is C_RETURN."
+        assert self.currentCmdType != CommandType.C_RETURN, \
+            "This method cannot be used if the current command type is C_RETURN.\n \
+                current command is {}".format(self.currentCmd)
         if self.currentCmdType == CommandType.C_ARITHMETIC:
             return self.currentCmd
-        elif self.currentCmdType == CommandType.C_PUSH:
-            return self.currentCmd.split(" ")[1]
-        elif self.currentCmdType == CommandType.C_POP:
+        elif self.currentCmdType in [CommandType.C_PUSH, CommandType.C_POP, CommandType.C_LABEL]:
             return self.currentCmd.split(" ")[1]
         else:
             raise AssertionError("An unknown command type was read.")
 
     def arg2(self) -> int:
-        assert self.currentCmdType not in [CommandType.C_PUSH, CommandType.C_POP, CommandType.C_FUNCTION, CommandType.C_CALL], \
+        assert self.currentCmdType in [CommandType.C_PUSH, CommandType.C_POP, CommandType.C_FUNCTION, CommandType.C_CALL], \
             "This method cannot be used if the current command type is not C_PUSH, C_POP, C_FUNCTION."
         if self.currentCmdType == CommandType.C_PUSH:
             return self.currentCmd.split(" ")[2]
