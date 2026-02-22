7c8fe99c7a8fa67ad530c3d79895ef6f76780127
peaceYoon <peace@peaceYoonui-MacBookPro.local>
2026-02-22 22:07:48 +0900

    됐다...

--- CodeWriter/codeWriter.py
+++ CodeWriter/codeWriter.py
@@ -10,7 +10,7 @@ class CodeWriter:
         self.__staticMap = {}
         self.__currentFileName = None
         # For FibonacciElement, StaticsTest
-        # self.__writeBootstrapCode()
+        self.__writeBootstrapCode()
 
     def updateFileName(self, filename:str):
         self.__currentFileName = filename
